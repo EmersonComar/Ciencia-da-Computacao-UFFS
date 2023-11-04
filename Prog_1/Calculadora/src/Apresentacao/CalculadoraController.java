0bc730ee52612a0bcbe3f57757b0fbb26bfdde06
Emerson <emerson.comar@estudante.uffs.edu.br>
2023-11-04 14:37:49 -0300

    Adicionando Calculadora

---
+++ Prog_1/Calculadora/src/Apresentacao/CalculadoraController.java
@@ -0,0 +1,57 @@
+package Apresentacao;
+
+import Calculos.Calcular;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class CalculadoraController {
+
+    @FXML
+    private Button bntCalcular;
+
+    @FXML
+    private Button bntCopiar;
+
+    @FXML
+    private Button bntLimpar;
+
+    @FXML
+    private ChoiceBox<?> chbOperacao;
+
+    @FXML
+    private Spinner<?> spnPrecisao;
+
+    @FXML
+    private TextArea txaResultado;
+
+    @FXML
+    private TextField txfPrimeiroValor;
+
+    @FXML
+    private TextField txfSegundoValor;
+
+    @FXML
+    void calcularResultado(ActionEvent event) {
+        double primeiroValor = Double.parseDouble(txfPrimeiroValor.getText());
+        double segundoValor = Double.parseDouble(txfSegundoValor.getText());
+        double resultado = Calcular.soma(primeiroValor, segundoValor, 5);
+        
+        txaResultado.setText(String.valueOf(resultado));
+    }
+
+    @FXML
+    void copiarResultado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void limparResultado(ActionEvent event) {
+        txaResultado.setText("");
+    }
+
+}
