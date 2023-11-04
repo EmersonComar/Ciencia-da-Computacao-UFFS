e46975831a3dd96e0611803cf1d1a0db36b45b41
Emerson <emerson.comar@estudante.uffs.edu.br>
2023-11-04 14:57:07 -0300

    Generalizando método para realizar as operações

--- Prog_1/Calculadora/src/Calculos/Calcular.java
+++ Prog_1/Calculadora/src/Calculos/Calcular.java
@@ -1,30 +1,9 @@
 package Calculos;
 
 import Calculos.Operacoes.Operacao;
-import Calculos.Operacoes.OperacoesBasicas.*;
 
 public class Calcular {
-    public static double soma(double valor1, double valor2, int precisao){
-        Operacao soma = new Soma();
-        double resultado = soma.executar(valor1, valor2, precisao);
-        return resultado;
-    }
-
-    public static double subtracao(double valor1, double valor2, int precisao){
-        Operacao Subtracao = new Subtracao();
-        double resultado = Subtracao.executar(valor1, valor2, precisao);
-        return resultado;
-    }
-
-    public static double divisao(double valor1, double valor2, int precisao){
-        Operacao Divisao = new Divisao();
-        double resultado = Divisao.executar(valor1, valor2, precisao);
-        return resultado;
-    }
-
-    public static double multiplicacao(double valor1, double valor2, int precisao){
-        Operacao Multiplicacao = new Multiplicacao();
-        double resultado = Multiplicacao.executar(valor1, valor2, precisao);
-        return resultado;
+    public static double realizarOperacao(Operacao operacao, double primeiroValor, double segundoValor, int precisao){
+        return operacao.executar(primeiroValor, segundoValor, precisao);
     }
 }
