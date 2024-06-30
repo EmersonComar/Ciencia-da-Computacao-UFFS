723aaa191a867e65eda0ffd5a2c51c909bafefd7
Emerson Comar <emerson.comar@estudante.uffs.edu.br>
2024-06-30 14:49:03 -0300

    Função cadastrar tarefa

--- 2_semestre/Estrutura_de_Dados/10_trabalho_final/programa.c
+++ 2_semestre/Estrutura_de_Dados/10_trabalho_final/programa.c
@@ -14,6 +14,34 @@ int menu(){
     return opt;
 }
 
+void cadastrarTarefa(Tree *sentinel){
+    int id, c;
+    char descricao[21];
+    float tempoLimite;
+
+    printf("\n--- Adicionado tarefa ---\n");
+    printf("Digite um valor inteiro para o ID da tarefa: ");
+    scanf("%d", &id);
+
+    if(consultarID(sentinel->root, id)){
+        printf("\nEste ID já está em uso. Informe um novo\n");
+        return;
+    }
+
+    printf("Digite uma breve descrição da tarefa: ");
+    fgets(descricao, sizeof(descricao), stdin);
+
+    while ((c = getchar()) != '\n' && c != EOF);
+
+    printf("Digite o tempo estimado para a realização da tarefa(em horas): ");
+    scanf("%f", &tempoLimite);
+
+    sentinel->root = adicionarTarefa(sentinel->root, criarTarefa(id, descricao, tempoLimite));
+
+    printf("\nTarefa cadastrada!\n");
+
+}
+
 int main(void){
     Tree sentinel;
     ListaOrdenada lista;
@@ -25,6 +53,16 @@ int main(void){
 
     do{
         opt = menu();
+
+        switch(opt){
+            case 1:
+                cadastrarTarefa(&sentinel);
+                break;
+            case 6:
+                break;
+            default:
+                printf("\nEscolha uma opção entre 1 e 6\n");
+        }
 
     }while (opt != 6);
 
