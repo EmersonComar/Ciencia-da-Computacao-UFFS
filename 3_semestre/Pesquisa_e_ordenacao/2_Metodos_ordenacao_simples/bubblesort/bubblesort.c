fc5b52ced1e9ae7ecb29d192c723f63eab84b511
Emerson Comar <emerson.comar@estudante.uffs.edu.br>
2024-09-07 12:16:50 -0300

    Implementacao basica Bubble Sort

---
+++ 3_semestre/Pesquisa_e_ordenacao/2_Metodos_ordenacao_simples/bubblesort/bubblesort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void exibirVetor(int *vetor, int N);
+void bubblesort(int *vetor, int N);
+
+int main(void){
+    int vetor[] = {1, 2, 3, 4, 5};
+
+    int N = sizeof(vetor)/sizeof(vetor[0]);
+
+    printf("Vetor desordenado: \n");
+    exibirVetor(vetor, N);
+
+    bubblesort(vetor, N);
+
+    printf("Vetor ordenado: \n");
+    exibirVetor(vetor, N);
+
+    return 0;
+}
+
+void exibirVetor(int *vetor, int N){
+    for(int i=0; i<N-1; i++)
+        printf("%d, ", vetor[i]);
+    printf("%d", vetor[N-1]);
+    printf("\n");
+}
+
+void bubblesort(int *vetor, int N){
+    for (int i= N-1; i>0; i--){
+        printf("Estado vetor:\n");
+        exibirVetor(vetor, N);
+        printf("\nPosição de i: %d\nValor de i: %d\n", i, vetor[i]);
+
+        for(int j=0; j<i; j++){
+            printf("\tPosição de j: %d\n", j);
+            printf("\t\tValor de vetor[j]: %d\n\t\tValor de vetor[j+1]: %d\n", vetor[j], vetor[j+1]);
+            if(vetor[j] > vetor[j+1]){
+                printf("\t\t\tTrocou %d por %d\n", vetor[j], vetor[j+1]);
+                int aux = vetor[j];
+                vetor[j] = vetor[j+1];
+                vetor[j+1] = aux;
+            }
+        }
+
+        printf("\n");
+    }
+}
