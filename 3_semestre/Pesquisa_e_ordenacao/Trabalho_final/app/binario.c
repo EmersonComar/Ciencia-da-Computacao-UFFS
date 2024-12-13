ac73530fdc6d104bb5d31ac76dc36adcf2915554
Emerson Comar <emerson.comar@estudante.uffs.edu.br>
2024-12-13 13:11:34 -0300

    Adicionado codigo para validar o binario e feito alguns ajustes basicos no huffman

---
+++ 3_semestre/Pesquisa_e_ordenacao/Trabalho_final/app/binario.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+void exibirBit(unsigned char byte){
+    for(int i = 7; i >= 0; i--){
+        printf("%d", (byte >> i) & 1);
+    }
+    printf(" ");
+}
+
+int main(int argc, char *argv[]){
+
+    if(argc != 2){
+        fprintf(stderr, "Uso: %s <arquivo_entrada>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *arquivo = fopen(argv[1], "rb");
+    if(arquivo == NULL){
+        fprintf(stderr, "Erro ao abrir arquivo %s\n", argv[1]);
+        return 1;
+    }
+
+    unsigned char byte;
+    while (fread(&byte, sizeof(unsigned char), 1, arquivo) == 1) {
+        exibirBit(byte);
+    }
+
+    printf("\n");
+    fclose(arquivo);
+    return 0;
+}
